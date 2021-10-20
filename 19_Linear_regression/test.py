cd4c42ed1d2e6e31bdb9a7596e575a0af211fd23
evrard dessain <evrard.dessain@gmail.com>
2021-10-20 14:53:54 +0200

    ex00 de 19

--- 19_Linear_regression/test.py
+++ 19_Linear_regression/test.py
@@ -4,3 +4,30 @@ import matplotlib.pyplot as plt
 
 from MyLinearRegression import MyLinearRegression as MyLR
 
+def predict_value(lr, X, Y):
+	pred =  lr.predict_(X)
+	return (pred)
+
+def train_model(lr, X, Y):
+	thet = lr.fit_(X, Y)
+	lr.theta = thet
+	return (thet)
+
+if __name__ == "__main__":
+	data = pd.read_csv("data.csv")
+	X = np.array(data[['km']]).reshape(-1,1)
+	Y = np.array(data[['price']]).reshape(-1,1)
+
+	lr = MyLR([0.0, 0.0])
+	
+	print("prediction without training the model :")
+	pred1 = predict_value(lr, X, Y)
+	print(pred1)
+
+	print("Theta after training the model :")
+	new_theta = train_model(lr, X, Y)
+	print(new_theta)
+
+	print("prediction using the new theta :")
+	pred2 = predict_value(lr, X, Y)
+	print(pred2)
